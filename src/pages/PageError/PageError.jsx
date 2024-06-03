42e307da82cee56d79ff05d3af658601f9582f9c
Chahinez125 <chahinez.semmar@yahoo.com>
2024-06-04 01:32:58 +0200

    page 'erreur fonctionnel avec navigation se direiger page acceuil

--- src/pages/PageError/PageError.jsx
+++ src/pages/PageError/PageError.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import { Link, } from 'react-router-dom'
+import './PageError.scss'
+function PageError(){
 
-export default function PageError() {
-  return (
-    <div>
-      
+return (
+  <>
+    <div className="wrap">
+     
+      <div className="error">
+        <h1>404</h1>
+        <p>Oups! La page que vous demandez n’existe pas.</p>
+        <Link className="return-link" to="/">
+          Retourner sur la page d’accueil
+        </Link>
+      </div>
     </div>
-  )
+   
+  </>
+)
 }
+export default PageError
