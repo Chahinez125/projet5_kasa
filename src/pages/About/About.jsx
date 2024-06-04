8d280bfd57d30e46388e33cd4aea5fc30b34ae40
Chahinez125 <chahinez.semmar@yahoo.com>
2024-06-04 22:54:01 +0200

    affichet img de la page acceuil et la description récupere de descriptionjson

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,9 +1,27 @@
 import React from 'react'
+import './About.scss'
+import Banner from '../../components/Banner/Banner';
+import mountain from '../../image/mountain.png'
+import dataAbout from '../../Data/description.json'
 
-export default function About() {
+const About = () => {
+
+  const documentTitle = "Kasa - A propos de nous";
+  document.title = documentTitle
   return (
-    <div>
-      About
+    <>
+   <Banner imageUrl={mountain} />
+   <div className='about'>
+    {dataAbout.map 
+    ((description, index) => (
+      <div key={index}>
+      <h2>{description.title}</h2>
+      <p>{description.content}</p>
     </div>
+
+    ))}
+   </div>
+    </>
   )
 }
+ export default About;
--- src/pages/Accomodation/Accomodation.jsx
+++ src/pages/Accomodation/Accomodation.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Accomodation() {
   return (
     <div>
-      
+   <p>Accomodation</p>
     </div>
   )
 }
